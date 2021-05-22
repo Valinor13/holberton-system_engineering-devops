fe312dc72f99448763f1a8a58adacc35daabf242
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-22 04:42:25 +0000

    Addding puppet config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+Host Holberton
+        HostName 3.84.179.220
+        IdentifyFile ~/.ssh/holberton
+        StrictHostKeyChecking no
+        User ubuntu
+        Port 22
+        PasswordAuthentication no
