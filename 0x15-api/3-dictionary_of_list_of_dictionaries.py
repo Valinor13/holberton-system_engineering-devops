bd6bed1d23b538b70abbf74ce7076da8f2082385
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-16 21:22:32 +0000

    Removing function declaration

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,28 +7,25 @@ import requests
 
 
 if __name__ == '__main__':
-    def save_tasks_to_json():
-        """ Get the employee tasks from API """
-        dic = {}
+    dic = {}
+    user_json = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+    todos_json = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
 
-        user_json = requests.get(
-            'https://jsonplaceholder.typicode.com/users/').json()
-        todos_json = requests.get(
-            'https://jsonplaceholder.typicode.com/todos/').json()
+    user_info = {}
+    for user in user_json:
+        user_info[user['id']] = user['username']
 
-        user_info = {}
-        for user in user_json:
-            user_info[user['id']] = user['username']
+    for task in todos_json:
+        if dic.get(task['userId'], False) is False:
+            dic[task['userId']] = []
+        task_dict = {}
+        task_dict['username'] = user_info[task['userId']]
+        task_dict['task'] = task['title']
+        task_dict['completed'] = task['completed']
+        dic[task['userId']].append(task_dict)
 
-        for task in todos_json:
-            if dic.get(task['userId'], False) is False:
-                dic[task['userId']] = []
-            task_dict = {}
-            task_dict['username'] = user_info[task['userId']]
-            task_dict['task'] = task['title']
-            task_dict['completed'] = task['completed']
-            dic[task['userId']].append(task_dict)
-
-        fn = "todo_all_employees.json"
-        with open(fn, 'w') as jf:
-            json.dump(dic, jf)
+    fn = "todo_all_employees.json"
+    with open(fn, 'w') as jf:
+        json.dump(dic, jf)
